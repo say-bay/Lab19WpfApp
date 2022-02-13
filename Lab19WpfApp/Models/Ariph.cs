e9d80b5259409e17690a0a20535f693536f30760
say-bay@mail.ru <say-bay@mail.ru>
2022-02-13 16:46:08 +0700

    Добавьте файлы проекта.

---
+++ Lab19WpfApp/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19WpfApp.Models
+{
+    static class Ariph
+    {
+        public static double СircF(double a) => (a * Math.PI * 2);
+    }
+}
